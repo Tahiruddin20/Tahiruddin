f7e0e09722e2ebb2c7aeec69b2803d1856cb6380
Tahiruddin20 <56099911+Tahiruddin20@users.noreply.github.com>
2019-11-15 11:50:16 +0800

    Add files via upload

---
+++ list.sh
@@ -0,0 +1,26 @@
+python nuyul.py +14253630425
+sleep 0
+python nuyul.py +6282125132349
+sleep 0
+python nuyul.py +12244800928
+sleep 0
+python nuyul.py +12342181860
+sleep 0
+python nuyul.py +14694152019
+sleep 0
+python nuyul.py +12485744178
+sleep 0
+python nuyul.py +15183107605
+sleep 0
+python nuyul.py +12485323976
+sleep 0
+python nuyul.py +12486943325
+sleep 0
+python nuyul.py +12483282046
+sleep 0
+python nuyul.py +12403395370
+sleep 0
+python nuyul.py +19368786335
+sleep 0
+python nuyul.py +15162186184
+sleep 0
